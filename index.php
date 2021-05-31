49b707f2fc9b32c7e2ad200de1c3178979f279f0
AnggitaTyasmara <anggitotsmr@gmail.com>
2021-05-31 19:45:21 +0700

    update

--- index.php
+++ index.php
@@ -4,15 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <!-- CDN dataTable -->
+    <link href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+    
+     <!-- Bootstrap CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    
     <title>Data Mahasiswa</title>
 </head>
-
-
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">Data Mahasiswa</a>
+            <a href="#" class="navbar-brand">Data Mahasiswa</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -25,42 +29,130 @@
             </div>
         </div>
     </nav>
+
     <div class="container data-mahasiswa mt-5">
-        <table class="table table-striped">
+
+         <!-- Button trigger modal -->
+         <button type="button" class="btn btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#tambahData">
+        Tambah Data
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="tambahData" tabindex="-1" aria-labelledby="tambahDataLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <!-- membuat form dengan method post untuk memanggil file create.php -->
+                    <form method="post" action="create.php" name="form">
+                    <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                    <div class="modal-body">
+                        <!-- form control -->
+                        <div class="mb-3">
+                            <!-- input nama -->
+                            <label for="nama" class="form-label">Nama</label>
+                            <input type="text" class="form-control" id="nama" placeholder="Masukkan Nama Siswa" name="nama" required>
+                        </div>
+                        <div class="mb-3">
+                            <!-- input nim -->
+                            <label for="nim" class="form-label">Nim</label>
+                            <input type="text" class="form-control" id="NIM" placeholder="Masukkan Nim Siswa" name="nim" required>
+                        </div>
+                        <div class="mb-3">
+                            <!-- input alamat -->
+                            <label for="alamat" class="form-label">Alamat</label>
+                            <textarea type="text" class="form-control" id="Alamat" placeholder="Masukkan Alamat Siswa" name="alamat" required></textarea>
+                        </div>
+                        <!-- akhir form control -->
+                    </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary" value="SIMPAN">Tambah</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!-- akhir modal -->
+
+        <table class="table table-striped" id="tabelMahasiswa">
             <thead>
                 <tr>
-                    <th scope="col">No. </th>
+                    <th scope="col">No.</th>
                     <th scope="col">Nama</th>
                     <th scope="col">NIM</th>
                     <th scope="col">Alamat</th>
                     <th scope="col">Aksi</th>
                 </tr>
             </thead>
+
             <tbody>
-                <?php  
+                <?php
+
+                // memanggil config.php 
                 include 'config.php';
+
+                // membuat variabel untuk nomor mahasiswa yang akan diincrement
                 $no = 1;
-                $mahasiswa = mysqli_query($koneksi, "select * from Mahasiswa");
-                while ($data = mysqli_fetch_array($mahasiswa)){
+
+                // melakukan query
+                $mahasiswa = mysqli_query($koneksi,"select * from mahasiswa");
+
+                // looping data mahasiswa
+                while($data = mysqli_fetch_array($mahasiswa)){
+        
                 ?>
+                    <!-- menampilkan data makasiswa ke dalam tabel -->
                     <tr>
-                        <td><?php echo $no++; ?></td>
-                        <td><?php echo $data['Nama']; ?></td>
-                        <td><?php echo $data['NIM']; ?></td>
-                        <td><?php echo $data['Alamat']; ?></td>
+                        <!-- increment nomor baris $no++ -->
+                        <td><?php echo $no++ ?></td>
+
+                        <!-- menampilkan data -->
+                        <td><?php echo $data['Nama'] ?></td>
+                        <td><?php echo $data['NIM'] ?></td>
+                        <td><?php echo $data['Alamat'] ?></td>
+
+                        <!-- kolom ini untuk aksi data mahasiswa -->
 
                         <td>
-                            <a href="edit.php?id=<<?php echo $data['Id']; ?>" class="btn btn-sm text-white">EDIT</a>
-                            <a href="delete.php?id=<?php echo $data['Id']; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Anda Yakin Akan Menghapus Data Mahasiswa Ini?')">HAPUS</a>
+                            <!-- aksi edit dan delete, di sini menggunakan btn-sm agar tombolnya kecil-->
+                            <!-- link untuk masuk ke halaman edit -->
+                            <!-- edit.php?id=<
+                            ?php echo $data['id']; ?> mengirim id data mahasiswa ke halaman edit -->
+                    
+                            <a href="edit.php?id=<?php echo $data['id']; ?>" 
+                            class="btn btn-warning btn-sm text-white">EDIT</a>
+
+                            <!-- link untuk delete berdasarkan id, akan keluar confirm terlebih dahulu -->
+
+                            <a href="delete.php?id=<?php echo $data['id']; ?>" 
+                            class="btn btn-danger btn-sm" onclick="return confirm('anda yakin akan menghapus data mahasiswa ini?')">HAPUS</a>
+                
                         </td>
                     </tr>
-                <?php                    
+            
+                <?php
                 }
                 ?>
+
             </tbody>
         </table>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-</body>
 
+    </div>
+    <!-- dataTable js -->
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap5.min.js"></script>
+
+    <script>
+        $(document).ready(function () {
+        $('#tabelMahasiswa').DataTable();
+        });
+    </script>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+</body>
 </html>
